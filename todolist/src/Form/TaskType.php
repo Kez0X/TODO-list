bc69fd710e59549db7366c136514395884a275a8
Luka <luka.baudrant@gmail.com>
2025-02-18 08:46:30 +0100

    Revert "feat : système de todo-list"
    This reverts commit 89bd720d402aebd41309d9ebe47d4a5e4f777abb.

--- todolist/src/Form/TaskType.php
+++ todolist/src/Form/TaskType.php
@@ -14,7 +14,7 @@ class TaskType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
+            ->add('title', TextType::class, [
                 'label' => 'Nom de la tâche'
             ])
             ->add('save', SubmitType::class, [
